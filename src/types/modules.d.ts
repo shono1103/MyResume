79a766ff0380f732a8c1c80a92a4cff0c0c04e6f
shonoshono <shono131103@gmail.com>
2026-02-15 14:04:21 +0900

    chore: typecheckをアプリコード対象に最適化

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -11,3 +11,24 @@ declare module '*.yaml' {
 declare module 'js-yaml' {
   export function load(input: string): unknown;
 }
+
+declare module '*.module.css' {
+  const classes: Record<string, string>;
+  export default classes;
+}
+
+declare module '@docusaurus/useBaseUrl' {
+  export default function useBaseUrl(path: string): string;
+}
+
+declare module '@docusaurus/Link' {
+  import * as React from 'react';
+  const Link: React.ComponentType<React.AnchorHTMLAttributes<HTMLAnchorElement> & {to?: string}>;
+  export default Link;
+}
+
+declare module '@theme-original/NavbarItem/ComponentTypes' {
+  import * as React from 'react';
+  const componentTypes: Record<string, React.ComponentType<any>>;
+  export default componentTypes;
+}
